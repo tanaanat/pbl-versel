cd8d193582476f16000efb286ccc5982f528fb59
tanaanat <takumadagu@gmail.com>
2024-07-16 13:18:31 +0900

    1

---
+++ src/pages/hello.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+//import { Box, Heading, Button, VStack, HStack, Text } from '@chakra-ui/react';
+
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+export default function Home() {
+    return (
+    <>
+    <h1>Hello</h1>
+    <h2>world</h2>
+    <h2>konnitiha</h2>
+    <Button colorScheme='blue'>Button</Button>    
+    </>
+    
+    )
+}
+/*const App = () => {
+    // ボタンがクリックされたときのハンドラ関数
+    const handleClick1 = () => {
+      alert('Button 1 clicked!');
+    };
+  
+    const handleClick2 = () => {
+      alert('Button 2 clicked!');
+    };
+  
+    return (
+      <Box textAlign="center" fontSize="xl">
+        <VStack spacing={8} mt={10}>
+          <Heading>Welcome to Chakra UI</Heading>
+          <Text>
+            This is an example of a simple webpage created with Chakra UI.
+          </Text>
+          <HStack spacing={4}>
+            <Button colorScheme="teal" size="lg" onClick={handleClick1}>
+              Button 1
+            </Button>
+            <Button colorScheme="blue" size="lg" onClick={handleClick2}>
+              Button 2
+            </Button>
+          </HStack>
+        </VStack>
+      </Box>
+    );
+  };
+  export default App;*/
+/*export default function Home() {
+    return (
+    <>
+    <h1>Hello</h1>
+    <h2>world</h2>
+    <h2>konnitiha</h2>
+    <Button colorScheme='blue'>Button</Button>    
+    </>
+    
+    )
+}*/
